03a91a188e91cc9b031cf77db0b15e143bd028fb
Aaahh Ahh <woohoomoo2u@gmail.com>
2014-11-14 22:13:47 -0500

    Update cm_bacon.mk

--- cm_bacon.mk
+++ cm_bacon.mk
@@ -20,54 +20,7 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from bacon device
-
-# Haters gonna hate..
-PRODUCT_CHARACTERISTICS := nosdcard
-
-# Ramdisk
-PRODUCT_PACKAGES += \
-    libinit_bacon \
-    fstab.bacon \
-    init.bacon.rc \
-    init.qcom.usb.rc \
-    ueventd.bacon.rc
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
-
-# NFC packages
-PRODUCT_PACKAGES += \
-    NfcNci \
-    Tag \
-    nfc_nci.bacon \
-    com.android.nfc_extras
-
-PRODUCT_PACKAGES += \
-    keystore.msm8084
-
-# NFC access control + feature files + configuration
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
-
-# System properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.default_network=20
-
-# call the proprietary setup
-$(call inherit-product-if-exists, vendor/oneplus/bacon/bacon-vendor.mk)
-
-ifneq ($(QCPATH),)
-$(call inherit-product-if-exists, $(QCPATH)/prebuilt_HY11/target/product/msm8974/prebuilt.mk)
-endif
-
-# Inherit from msm8974-common
-$(call inherit-product, device/oppo/msm8974-common/msm8974.mk)
+$(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit some common CM stuff
 #$(call inherit-product, vendor/cm/config/gsm.mk)
